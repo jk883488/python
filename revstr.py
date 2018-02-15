306250ad23b5a163d47e6ec0490c0b0b10b8ce71
jk883488 <36299416+jk883488@users.noreply.github.com>
2018-02-15 14:11:45 +0530

    to reverse a string

---
+++ revstr.py
@@ -0,0 +1,11 @@
+def reverse(string):
+	string = "".join(reversed(string))
+	return string
+
+s = "earth is beauty full"
+
+print ("The original string is : ",end="")
+print (s)
+
+print ("The reversed string(using reversed) is : ",end="")
+print (reverse(s))

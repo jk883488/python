c436197822dafc64e6999c6d85f19f7fd0230c62
jk883488 <36299416+jk883488@users.noreply.github.com>
2018-02-22 17:52:01 +0530

    to find factorial
    to find factorial of the no bellow 20

--- factnum.py
+++ factnum.py
@@ -3,6 +3,8 @@ num = 7
 factorial = 1
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
+if num > 20:   
+   print("Sorry, you reached the limit")
 elif num == 0:
    print("The factorial of 0 is 1")
 else:
